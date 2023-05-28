786cc978fec1a495a5bb926d5589b46e7dc794f7
NexusI9 <nassim.elkhantour@gmail.com>
2023-05-27 23:43:14 -0400

    header improvement

--- public/mail/index.php
+++ public/mail/index.php
@@ -8,23 +8,38 @@
         $data = json_decode($json);
 
         $content = $data->content;
-        $email = $data->email;
+        $from = $data->email;
         $name = $data->name;
-
+        $to = "nassim@elkhantour.com";
 
         //clean Content 
         $content =  str_replace("\n.", "\n..",$content);
         $content =  wordwrap($content, 70, "\r\n");
 
-        $to = "nassim@elkhantour.com";
-        $subject = "[Contact Form] ".$name." (".$email.")";
-        $headers = array(
-            'From' => $email,
-            'Reply-To' => $to,
-            'X-Mailer' => 'PHP/' . phpversion()
+
+        $encoding = "utf-8";
+        $subject = "Incoming message from:". $name;
+
+        $subject_preferences = array(
+            "input-charset" => $encoding,
+            "output-charset" => $encoding,
+            "line-length" => 76,
+            "line-break-chars" => "\r\n"
         );
 
-        if($content && $email && $name){
+
+        $headers = "From: ".$from." <".$from."> \r\n";
+        $headers .= "Reply-To:".$to."\r\n";
+        $headers .= "Return-Path:".$to."\r\n";
+        $headers .= "MIME-Version: 1.0 \r\n";
+        $headers .= "Content-Transfer-Encoding: 8bit \r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers .= "Date: ".date("r (T)")." \r\n";
+        $headers .= "X-Mailer: PHP/" . phpversion();
+        $headers .= iconv_mime_encode("Subject", $subject, $subject_preferences);
+
+
+        if($content && $from && $name){
 
             $mail =  mail( $to, $subject, $content, $headers);
 
