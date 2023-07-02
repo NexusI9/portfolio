fd39a923f7979610f5443158122c0ca49608d059
NexusI9 <nassim.elkhantour@gmail.com>
2023-07-02 11:20:22 -0400

    fix contact text

--- src/components/Layout/ContactLayout.js
+++ src/components/Layout/ContactLayout.js
@@ -46,11 +46,11 @@ export default ({ main = true }) => {
             <div>
                 {main && <>
                     <h1>Let's get <wbr /> in touch!</h1>
-                    <h2>Interested in working with me? Simply want to say hi? <br/> Feel free to reach out using the form or my email below. </h2>
+                    <h2>Interested in working with me? Simply want to say hi?<br/>Feel free to reach out using the form or my email below.</h2>
                 </>}
                 {!main && <>
                     <h3>Let's get <wbr /> in touch!</h3>
-                    <h4>Interested in working with me, or simply want to say hi? Feel free to reach out using the form or my email below. </h4>
+                    <h4>Interested in working with me? Simply want to say hi?<br/>Feel free to reach out using the form or my email below.</h4>
                 </>
                 }
 
