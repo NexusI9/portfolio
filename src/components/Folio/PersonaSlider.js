30978e2056ea34695ca87f60c02640f48b53bde4
NexusI9 <nassim.elkhantour@gmail.com>
2023-08-28 23:38:35 -0400

    manual drag slider

--- src/components/Folio/PersonaSlider.js
+++ src/components/Folio/PersonaSlider.js
@@ -1,7 +1,31 @@
 import Persona from "./Persona"
+import { useEffect, useRef, useState } from "react"
+import { motion } from "framer-motion";
 
-export default ({personas}) => (
-    <div className='persona-slider'>
-        {personas.map( (p,i) => <Persona key={`${p.name||i}personacard`} {...p}/>)}
-    </div>
-)
+
+export default ({ personas }) => {
+
+    const slider = useRef();
+    const [width, setWidth] = useState();
+
+    useEffect(() => {
+       if(slider.current){
+        setWidth( slider.current.getBoundingClientRect().width );
+       }
+    }, []);
+
+    return (
+        <motion.div
+            className='persona-slider'
+            ref={slider}
+        >
+            {       console.log(width)}
+            <motion.div
+                drag='x'
+                dragConstraints={{left:-1*width/1.55, right: 0}}
+            >
+                {personas.map((p, i) => <Persona key={`${p.name || i}personacard`} {...p} />)}
+            </motion.div>
+        </motion.div>
+    )
+};
