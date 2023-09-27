a988369cc105c0fc0e5f9747f90a7f9b00a596b5
NexusI9 <nassim.elkhantour@gmail.com>
2023-09-27 12:27:06 +0200

    fix skin contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,16 @@
 import Head from 'next/head';
 import { ContactLayout } from '@/components/Layout';
 import { Signature } from '@/components/Statics';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 export default function Contact() {
+
+  const dispatch = useDispatch();
+  
+  useEffect(()=>{
+    dispatch({type:'SWITCH_SKIN',skin:'default'});
+  },[]);
 
   return (
     <>
