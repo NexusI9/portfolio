f4caedf65070ddbdc9c7e1f7d772690d00408841
NexusI9 <nassim.elkhantour@gmail.com>
2023-09-30 22:02:36 +0200

    update gallery

--- src/projects/aciddesign/gallery.js
+++ src/projects/aciddesign/gallery.js
@@ -1,15 +1,27 @@
 export default {
-    holospace:[
+    holospace_product:[
         { type:'base', folder:'/aciddesign/holospace/g1', pictures:['holo_1', 'holo_2', 'holo_3'] },
-        { type:'split', folder:'/aciddesign/holospace/g2', pictures:['holo_4','holo_5']}
       ],
+    holospace_branding:[
+      { type:'split', folder:'/aciddesign/holospace/g2', pictures:['holo_4','holo_5']},
+      { type:'base', folder:'/aciddesign/holospace/g3', pictures:['trilobeat-1']},
+      { type:'base', folder:'/aciddesign/holospace/g3', pictures:['trilobeat-2']},
+      { type:'base', folder:'/aciddesign/holospace/g3', pictures:['trilobeat-3']},
+      { type:'base', folder:'/aciddesign/holospace/g3', pictures:['trilobeat-4']},
+      { type:'base', folder:'/aciddesign/holospace/g3', pictures:['trilobeat-5']},
+      { type:'base', folder:'/aciddesign/holospace/g3', pictures:['trilobeat-6']},
+      { type:'base', folder:'/aciddesign/holospace/g3', pictures:['trilobeat-7']},
+      { type:'base', folder:'/aciddesign/holospace/g3', pictures:['trilobeat-8']},
+    ],
     comet:[
       { type:'base', folder:'/aciddesign/thecomet/g1', pictures:['comet_1', 'comet_2', 'comet_5'] },
       { type:'split', folder:'/aciddesign/thecomet/g2', pictures:['comet_3','comet_4']}
     ],
-    spicejam:[
+    spicejam_packaging:[
         { type:'base', folder:'/aciddesign/spicejam/g1', pictures:['sp_0', 'sp_1', 'sp_2'] },
         { type:'split', folder:'/aciddesign/spicejam/g2', pictures:['sp_4','sp_5','sp_6','sp_7']},
+      ],
+      spicejam_branding:[
         { type:'invertedbase', folder:'/aciddesign/spicejam/g3', pictures:['sp_8','sp_9','sp_10']}
       ],
     eden:[
--- src/projects/aciddesign/index.js
+++ src/projects/aciddesign/index.js
@@ -1,57 +1,71 @@
-import {Article, Body, Title, Gallery} from '../../components/Folio';
+import { Article, Body, Title, Gallery, Space } from '../../components/Folio';
 import galleryRoute from './gallery';
 
 export default () => (
-    <>
-        <Article spaced={true} name='#01 Holospace'>
-          <Title
-            label='#01 Holospace' 
-            summary="Sold by Trilobeat, the Holospace is a holographic audio speaker projecting soundwaves and a spectrum of others visuals according to the user's music playing"
-            className='half' 
-          />
-          <Body flexDirection='vertical' >
-            <Gallery galleries={galleryRoute} galleryKey='holospace'/>
-          </Body>
-        </Article>
+  <>
+    <Article spaced={true} name='#01 Holospace'>
+      <Title
+        label='Holospace'
+        summary="Sold by Trilobeat, the Holospace is a holographic audio speaker projecting soundwaves and a spectrum of others visuals according to the user's music playing"
+        className='half'
+      />
+      <Body flexDirection='vertical' >
+        <h4>Product Design & Packaging</h4>
+        <Gallery galleries={galleryRoute} galleryKey='holospace_product' />
+      </Body>
+      <Space type='small' />
+      <Body flexDirection='vertical' >
+        <h4>Branding</h4>
+        <Gallery galleries={galleryRoute} galleryKey='holospace_branding' />
+      </Body>
+    </Article>
 
-        <Article spaced={true} name='#02 The Comet'>
-        <Title
-            label='#02 The Comet'
-            summary="The Comet is galaxy's largest Space community and Astrophysics news feed." 
-            className='half' 
-          />
-          <Body flexDirection='vertical' >
-            <Gallery galleries={galleryRoute} galleryKey='comet'/>
-          </Body>
-        </Article>
+    <Article spaced={true} name='#02 The Comet'>
+      <Title
+        label='The Comet'
+        summary="The Comet is galaxy's largest Space community and Astrophysics news feed."
+        className='half'
+      />
+      <Body flexDirection='vertical' >
+        <h4>App UX/UI Design</h4>
+        <Gallery galleries={galleryRoute} galleryKey='comet' />
+      </Body>
+    </Article>
 
-        <Article spaced={true} name='#03 Spice Jam'>
-        <Title
-            label='#03 SPICE JAM'
-            summary="Famously known for it's top quality psychotropic brewings and herbal blends, Spice Jam is a nutrition company making it's consummer dream and transcend since 1969."
-            className='half' 
-          />
-          <Body flexDirection='vertical' >
-            <Gallery galleries={galleryRoute} galleryKey='spicejam'/>
-          </Body>
-        </Article>
-
-
-        <Article spaced={true} name='#04 3DEN'>
-        <Title
-            label='#04 3DEN'
-            summary={
-            <>
-              Since 3045, 3DEN has seen itself becoming the #1 electronic retailers of erotic and adult products.
-              Selling a variety of goods going from top quality Simstims* to androids, 3DEN is a place where everyone can unleash their deepest, and way too often, darkest pleasures.
-            </>}
-            className='half' 
-          />
-          <Body flexDirection='vertical' >
-            <Gallery galleries={galleryRoute} galleryKey='eden'/>
-          </Body>
-        </Article>
+    <Article spaced={true} name='#03 Spice Jam'>
+      <Title
+        label='SPICE JAM'
+        summary="Famously known for it's top quality psychotropic brewings and herbal blends, Spice Jam is a nutrition company making it's consummer dream and transcend since 1969."
+        className='half'
+      />
+      <Body flexDirection='vertical' >
+        <h4>Packaging & collaterals</h4>
+        <Gallery galleries={galleryRoute} galleryKey='spicejam_packaging' />
+      </Body>
+      <Space type='small'/>
+      <Body flexDirection='vertical' >
+        <h4>Logotype & application</h4>
+        <Gallery galleries={galleryRoute} galleryKey='spicejam_branding' />
+      </Body>
+    </Article>
 
 
-    </>
-  );
+    <Article spaced={true} name='#04 3DEN'>
+      <Title
+        label='3DEN'
+        summary={
+          <>
+            Since 3045, 3DEN has seen itself becoming the #1 electronic retailers of erotic and adult products.
+            Selling a variety of goods going from top quality Simstims* to androids, 3DEN is a place where everyone can unleash their deepest, and way too often, darkest pleasures.
+          </>}
+        className='half'
+      />
+      <Body flexDirection='vertical' >
+        <h4>Logotype & collaterals</h4>
+        <Gallery galleries={galleryRoute} galleryKey='eden' />
+      </Body>
+    </Article>
+
+
+  </>
+);
