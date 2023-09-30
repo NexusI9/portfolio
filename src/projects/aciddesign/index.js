74fbac0b935be4c355faafd2d516f399efe7458d
NexusI9 <nassim.elkhantour@gmail.com>
2023-09-30 14:03:26 +0200

    Update 3den

--- src/projects/aciddesign/index.js
+++ src/projects/aciddesign/index.js
@@ -1,31 +1,7 @@
 import {Article, Body, Title, Gallery} from '../../components/Folio';
+import galleryRoute from './gallery';
 
-export default () => {
-
-  const galleryRoute = {
-    holospace:[
-        { type:'base', folder:'/aciddesign/holospace/g1', pictures:['holo_1', 'holo_2', 'holo_3'] },
-        { type:'split', folder:'/aciddesign/holospace/g2', pictures:['holo_4','holo_5']}
-      ],
-    comet:[
-      { type:'base', folder:'/aciddesign/thecomet/g1', pictures:['comet_1', 'comet_2', 'comet_5'] },
-      { type:'split', folder:'/aciddesign/thecomet/g2', pictures:['comet_3','comet_4']}
-    ],
-    spicejam:[
-        { type:'base', folder:'/aciddesign/spicejam/g1', pictures:['sp_0', 'sp_1', 'sp_2'] },
-        { type:'split', folder:'/aciddesign/spicejam/g2', pictures:['sp_4','sp_5','sp_6','sp_7']},
-        { type:'invertedbase', folder:'/aciddesign/spicejam/g3', pictures:['sp_8','sp_9','sp_10']}
-      ],
-    eden:[
-      { type:'base', folder:'/aciddesign/3den/g1', pictures:['3den_1', '3den_7', '3den_8'] },
-      { type:'split', folder:'/aciddesign/3den/g2', pictures:['3den_6','3den_5']},
-    ]
-  };
-
-
-
-
-  return(
+export default () => (
     <>
         <Article spaced={true} name='#01 Holospace'>
           <Title
@@ -79,4 +55,3 @@ export default () => {
 
     </>
   );
-}
